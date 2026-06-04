1ae21984806044463e8d949ff776ba4d995d85c5
Noel Nunes <noelnunes1200@gmail.com>
2026-06-04 17:10:53 +0000

    added fixes

--- models/silver/stg_taxi_trips.sql
+++ models/silver/stg_taxi_trips.sql
@@ -1,19 +1,13 @@
+{{
+    config(schema="silver")
+}}
 WITH taxi_trips as (
     SELECT
-        md5(
-            concat_ws(
-                '|',
-                VendorID,
+        ROW_NUMBER() OVER (
+            ORDER BY
                 tpep_pickup_datetime,
-                tpep_dropoff_datetime,
-                trip_distance,
-                pickup_longitude,
-                pickup_latitude,
-                dropoff_longitude,
-                dropoff_latitude
-            )
-        ) AS trip_hash,
-
+                tpep_dropoff_datetime
+        ) AS trip_id,
         VendorID,
         CAST(tpep_pickup_datetime AS TIMESTAMP) AS tpep_pickup_datetime,
         CAST(tpep_dropoff_datetime AS TIMESTAMP) AS tpep_dropoff_datetime,
@@ -25,6 +19,8 @@ WITH taxi_trips as (
         CAST(dropoff_latitude AS DOUBLE) AS dropoff_latitude,
         CAST(fare_amount AS DECIMAL(10,2)) AS fare_amount,
         CAST(total_amount AS DECIMAL(10,2)) AS total_amount,
+        COALESCE(CAST(RateCodeID As STRING), "-") AS rate_code_id,
+        CAST(payment_type AS INT) AS payment_type,
         inserted_at
 
     FROM 
