d9ee22650135a38765faa5f4463ca6b09b01e78b
Noel Nunes <noelnunes1200@gmail.com>
2026-06-04 15:46:04 +0000

    added stg taxi_trips

---
+++ models/silver/stg_taxi_trips.sql
@@ -0,0 +1,32 @@
+WITH (
+    SELECT
+        md5(
+            concat_ws(
+                '|',
+                VendorID,
+                tpep_pickup_datetime,
+                tpep_dropoff_datetime,
+                trip_distance,
+                pickup_longitude,
+                pickup_latitude,
+                dropoff_longitude,
+                dropoff_latitude
+            )
+        ) AS trip_hash,
+        
+        VendorID,
+        CAST(tpep_pickup_datetime AS TIMESTAMP) AS tpep_pickup_datetime,
+        CAST(tpep_dropoff_datetime AS TIMESTAMP) AS tpep_dropoff_datetime,
+        CAST(passenger_count AS INT) AS passenger_count,
+        CAST(trip_distance AS DOUBLE) AS trip_distance,
+        CAST(pickup_longitude AS DOUBLE) AS pickup_longitude,
+        CAST(pickup_latitude AS DOUBLE) AS pickup_latitude,
+        CAST(dropoff_longitude AS DOUBLE) AS dropoff_longitude,
+        CAST(dropoff_latitude AS DOUBLE) AS dropoff_latitude,
+        CAST(fare_amount AS DECIMAL(10,2)) AS fare_amount,
+        CAST(total_amount AS DECIMAL(10,2)) AS total_amount,
+        inserted_at
+
+    FROM 
+        {{ source('bronze', 'trips') }}
+)
